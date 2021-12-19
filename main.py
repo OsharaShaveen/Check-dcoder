d26666d724d6c860fa2567e5aec95946468fc70c
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2021-12-19 19:44:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import os
 import telebot
 
 
-bot = telebot.TeleBot("5092293428:AAEViJbsdhTHIqcLU_WZNr_v-U3cB8DqKdQ")
+bot = telebot.TeleBot("5034975242:AAEXLY_YNKTo3D8t0j8PZDeVw6Pn_t6wSmE")
 
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Ehi Download Bot Made By Using Python3")
+  bot.reply_to(message, "Hello! I'm Chat Bot Made By Using Python3")
 
 
 @bot.message_handler(commands=["info"])
 def send_message(message):
-  bot.send_message(message.chat.id, "Im Ehi Bot, You Can Download Ehi Files Using Me.. 😇 My Master Is WhiteDevil, He Makes Me Using Python3, Made In Sri lanka 🇱🇰")
+  bot.send_message(message.chat.id, "Im Ehi Bot, You Can Chat  Me.. 😇 My Master Is WhiteDevil, He Makes Me Using Python3, Made In Sri lanka 🇱🇰")
 
 
 
